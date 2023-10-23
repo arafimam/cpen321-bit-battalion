41f5d3c8728c518f1d4c29d9e71e4a7e954c9db3
Harshil Patel <harshil11100195@gmail.com>
2023-10-23 16:08:55 -0700

    small commit for adding and deleting users

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -10,14 +10,22 @@ const userSchema = new mongoose.Schema({
     required: true,
     unique: true,
   },
-  userLocation: {
-    location: {
-      latitude: { type: Number, required: true },
-      longitude: { type: Number, required: true },
-    },
-    locationName: { type: String, required: true },
-  },
+  // userLocation: {
+  //   location: {
+  //     latitude: { type: Number, required: true },
+  //     longitude: { type: Number, required: true },
+  //   },
+  //   locationName: { type: String, required: true },
+  // },
 });
+
+userSchema.statics.createUser = async function (username, googleId) {
+  this.create({ username, googleId });
+};
+
+userSchema.statics.deleteUser = async function (googleId) {
+  this.findOneAndDelete({ googleId });
+};
 
 const User = mongoose.model("User", userSchema);
 
