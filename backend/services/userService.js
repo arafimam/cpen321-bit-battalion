5757691086f83084f58ef9d635386df4cff1a158
Ritam Singal <ritamsingal@gmail.com>
2023-10-29 15:35:12 -0700

    Tyring user auth

--- backend/services/userService.js
+++ backend/services/userService.js
@@ -1,2 +1,16 @@
-// This will have CRUD functions
-// Will be called by controller and the service itself will call functions in the model (actual db calls)
+// const { OAuth2Client } = require('google-auth-library');
+// const client = new OAuth2Client();
+
+// async function verify() {
+//   const ticket = await client.verifyIdToken({
+//     idToken: token,
+//     audience: CLIENT_ID // Specify the CLIENT_ID of the app that accesses the backend
+//     // Or, if multiple clients access the backend:
+//     //[CLIENT_ID_1, CLIENT_ID_2, CLIENT_ID_3]
+//   });
+//   const payload = ticket.getPayload();
+//   const userid = payload['sub'];
+//   // If request specified a G Suite domain:
+//   // const domain = payload['hd'];
+// }
+// verify().catch(console.error);
