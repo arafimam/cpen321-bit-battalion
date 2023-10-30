f3154b01e50ac09976a45ba00d9b5b607f7c2fd8
Ritam Singal <ritamsingal@gmail.com>
2023-10-29 19:42:34 -0700

    Lists controller, servive, and model complete. Co-authored-by: harshil-patel11 <harshil-patel11@users.noreply.github.com>

--- backend/models/listModel.js
+++ backend/models/listModel.js
@@ -41,34 +41,15 @@ const listSchema = new mongoose.Schema({
 
 const List = mongoose.model('List', listSchema);
 
-async function getAll() {
-  return await List.find({});
-}
-
-async function getById(listId) {
-  return await List.findById(listId);
-}
-
-async function addPlace(place, listId) {
-  const filter = { _id: listId };
-  // TODO: Add check to not add duplicates
-  const update = { $push: { places: place } };
+async function createList(listName) {
+  const list = {
+    listName
+  };
 
   try {
-    return await List.findOneAndUpdate(filter, update, { new: true });
+    return await List.create(list);
   } catch (error) {
-    throw new Error('Error in DB while adding place to list: ' + error.message);
-  }
-}
-
-async function deletePlace(listId, placeId) {
-  const filter = { _id: listId };
-  const update = { $pull: { places: placeId } };
-
-  try {
-    return await List.updateOne(filter, update, { new: true });
-  } catch (error) {
-    throw new Error('Error in DB while deleting place from the list: ' + error.message);
+    throw new Error('Error in DB while creating list: ' + error.message);
   }
 }
 
@@ -80,4 +61,31 @@ async function deleteList(listId) {
   }
 }
 
-module.exports = { List, getById, deleteList, addPlace, deletePlace, getAll };
+async function getListById(listId) {
+  return await List.findById(listId);
+}
+
+async function addPlaceToList(listId, place) {
+  const filter = { _id: listId };
+  // TODO: Add check to not add duplicates
+  const update = { $push: { places: place } };
+
+  try {
+    return await List.findOneAndUpdate(filter, update, { new: true });
+  } catch (error) {
+    throw new Error('Error in DB while adding place to list: ' + error.message);
+  }
+}
+
+async function removePlaceFromList(listId, placeId) {
+  const filter = { _id: listId };
+  const update = { $pull: { places: { placeId: placeId } } };
+
+  try {
+    return await List.updateOne(filter, update, { new: true });
+  } catch (error) {
+    throw new Error('Error in DB while deleting place from the list: ' + error.message);
+  }
+}
+
+module.exports = { List, getListById, deleteList, addPlaceToList, removePlaceFromList, createList };
