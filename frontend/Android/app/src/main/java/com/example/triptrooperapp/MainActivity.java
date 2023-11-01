11dc9cd0df65dfcc293f3098255511882eb38967
arafimam -Git <syedarafimam27@gmail.com>
2023-11-01 01:43:22 -0700

    Cleanup

--- frontend/Android/app/src/main/java/com/example/triptrooperapp/MainActivity.java
+++ frontend/Android/app/src/main/java/com/example/triptrooperapp/MainActivity.java
@@ -166,7 +166,7 @@ public class MainActivity extends AppCompatActivity {
                         // Log and toast
 
                         Log.d(TAG, token);
-                        Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
+                        //Toast.makeText(MainActivity.this, token, Toast.LENGTH_SHORT).show();
                         //TODO:Send token to backend here
                     }
                 });
