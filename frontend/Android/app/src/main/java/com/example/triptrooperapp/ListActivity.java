54a2c4898a2bfc9b5a5e9275226f94ecdf81b646
arafimam -Git <syedarafimam27@gmail.com>
2023-11-08 19:22:02 -0800

    Added Error Handling paths for list activity

--- frontend/Android/app/src/main/java/com/example/triptrooperapp/ListActivity.java
+++ frontend/Android/app/src/main/java/com/example/triptrooperapp/ListActivity.java
@@ -7,6 +7,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
@@ -26,7 +27,8 @@ import okhttp3.Request;
 import okhttp3.Response;
 
 /**
- * List screen.
+ * List screen for user.
+ * Accessed via the bottom navigation bar.
  */
 public class ListActivity extends AppCompatActivity {
 
@@ -37,41 +39,63 @@ public class ListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
 
-        listBoxContainer = findViewById(R.id.list_layout_container);
-        retrieveListForUser();
+        NetworkChecker networkChecker = new NetworkChecker(ListActivity.this);
+        if (!networkChecker.haveNetworkConnection()) {
+            handleNoConnection();
+            checkEmptyListView();
+        } else {
+            listBoxContainer = findViewById(R.id.list_layout_container);
+            retrieveListForUser();
+            setActionForAddListButton();
+        }
+    }
 
+    /**
+     * Handle no internet connection.
+     */
+    private void handleNoConnection() {
+        AlertDialog.Builder builder =
+                new AlertDialog.Builder(ListActivity.this);
+        builder.setMessage(
+                        "Unable to retrieve your lists. Try again later.")
+                .setTitle(
+                        "No internet.");
+        builder.create().show();
+    }
 
-        /**
-         * Create List, Floating action Button.
-         */
+    /**
+     * Sets up everything for the floating action button for
+     * adding lists.
+     * When clicked opens up a alert dialog that shows
+     * "List name" text field and create list button.
+     */
+    private void setActionForAddListButton() {
         FloatingActionButton createListButton = findViewById(R.id.create_list);
         createListButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                // create the alert dialog for create list.
                 AlertDialog.Builder builder =
                         new AlertDialog.Builder(ListActivity.this);
                 View dialogView =
                         LayoutInflater.from(ListActivity.this).inflate(
                                 R.layout.create_list_dialog_view, null);
+
+                // set up the text field and create list btn.
                 final EditText listNameText =
                         dialogView.findViewById(R.id.list_name_textField);
                 GreenButtonView createListButton =
                         dialogView.findViewById(R.id.create_list_button);
                 createListButton.setButtonText("Create List");
                 builder.setView(dialogView);
-
                 final AlertDialog dialog = builder.create();
                 createListButton.setButtonActionOnClick(new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        if (listNameText.getText().toString().equals("")) {
-                            Toast.makeText(ListActivity.this, "List Name " +
-                                    "Empty", Toast.LENGTH_SHORT).show();
-                            dialog.dismiss();
-                        } else {
-                            createListByUser(listNameText.getText().toString());
-                            dialog.dismiss();
-                        }
+                        handleErrorForCreateList(
+                                listNameText.getText().toString(),
+                                dialog
+                        );
                     }
                 });
                 dialog.show();
@@ -80,6 +104,26 @@ public class ListActivity extends AppCompatActivity {
         });
     }
 
+    /**
+     * Checks if list name is empty.
+     * If empty show a toast message
+     * If not creates a list.
+     */
+    private void handleErrorForCreateList(String enteredListName,
+                                          AlertDialog dialog) {
+        if (enteredListName.isEmpty()) {
+            Toast.makeText(ListActivity.this,
+                            "No list name entered.", Toast.LENGTH_LONG)
+                    .show();
+        } else {
+            createListByUser(enteredListName);
+            dialog.dismiss();
+        }
+    }
+
+    /**
+     * retrieves list for user.
+     */
     private void retrieveListForUser() {
         GoogleSignInAccount account =
                 GoogleSignIn.getLastSignedInAccount(ListActivity.this);
@@ -100,48 +144,90 @@ public class ListActivity extends AppCompatActivity {
                         for (int i = 0; i < listArray.length(); i++) {
                             try {
                                 JSONObject list = listArray.getJSONObject(i);
-                                ListBoxComponentView listBox =
-                                        new ListBoxComponentView(ListActivity.this);
                                 final String listName = list.getString(
                                         "listName");
                                 final String listId = list.getString("_id");
-                                listBox.setMainTitleText(listName);
-                                listBox.setVisibilityOfTextViews(View.VISIBLE
-                                        , View.INVISIBLE, View.INVISIBLE);
-                                listBox.setActionOnCardClick(new View.OnClickListener() {
-                                    @Override
-                                    public void onClick(View view) {
-                                        Intent intent =
-                                                new Intent(ListActivity.this,
-                                                        ListDetailsActivity.class);
-                                        intent.putExtra("listName", listName);
-                                        intent.putExtra("id", listId);
-                                        intent.putExtra("context", "userList");
-                                        startActivity(intent);
-                                    }
-                                });
+                                ListBoxComponentView listBox =
+                                        createListBoxForEachList(listName,
+                                                listId);
                                 listBoxContainer.addView(listBox);
                             } catch (JSONException e) {
                                 throw new CustomException("error", e);
                             }
                         }
+                        checkEmptyListView();
                     });
 
-                } catch (IOException e) {
-                    throw new CustomException("error", e);
-                } catch (JSONException e) {
+                } catch (IOException | JSONException e) {
                     throw new CustomException("error", e);
                 }
-            } else {
+            }
+
+            // if response is not successful.
+            else {
                 try {
                     Log.d("TAG", response.body().string());
                 } catch (IOException e) {
                     throw new CustomException("error", e);
                 }
+                handleErrorForRetrievingUserList();
             }
         }).start();
     }
 
+
+    /**
+     * Creates list box for each user.
+     * Returns Null if listName or listId is Null.
+     */
+    private ListBoxComponentView createListBoxForEachList(
+            String listName,
+            String listId
+    ) {
+        if (listName.isEmpty() || listId.isEmpty()) {
+            return null;
+        }
+
+        ListBoxComponentView listBox =
+                new ListBoxComponentView(ListActivity.this);
+        listBox.setMainTitleText(listName);
+        listBox.setVisibilityOfTextViews(View.VISIBLE
+                , View.INVISIBLE, View.INVISIBLE);
+        listBox.setActionOnCardClick(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent =
+                        new Intent(ListActivity.this,
+                                ListDetailsActivity.class);
+                intent.putExtra("listName", listName);
+                intent.putExtra("id", listId);
+                intent.putExtra("context", "userList");
+                startActivity(intent);
+            }
+        });
+
+        return listBox;
+    }
+
+    /**
+     * Handle error in retrieving user list.
+     * Shows an error dialog.
+     */
+    private void handleErrorForRetrievingUserList() {
+        runOnUiThread(() -> {
+            AlertDialog.Builder builder =
+                    new AlertDialog.Builder(ListActivity.this);
+            builder.setMessage(
+                            "Unable to retrieve your lists. Try again later.")
+                    .setTitle(
+                            "Something went wrong.");
+            builder.create().show();
+        });
+    }
+
+    /**
+     * Creates list by user.
+     */
     private void createListByUser(String listName) {
         GoogleSignInAccount account =
                 GoogleSignIn.getLastSignedInAccount(ListActivity.this);
@@ -161,7 +247,8 @@ public class ListActivity extends AppCompatActivity {
                 runOnUiThread(() -> {
                     Toast.makeText(ListActivity.this,
                             "Created list " + listName, Toast.LENGTH_SHORT).show();
-                    recreate();
+                    refreshListView();
+                    checkEmptyListView();
                 });
             } else {
                 try {
@@ -172,5 +259,27 @@ public class ListActivity extends AppCompatActivity {
             }
         }).start();
 
+    }
+
+    /**
+     * Refreshes the list view.
+     */
+    private void refreshListView() {
+        listBoxContainer.removeAllViews();
+        retrieveListForUser();
+    }
+
+    /**
+     * Checks for empty list view.
+     * If no list added shows a message and a
+     * error logo.
+     */
+    private void checkEmptyListView() {
+        TextView noListText = findViewById(R.id.textView_noList);
+        if (listBoxContainer.getChildCount() == 0) {
+            noListText.setVisibility(View.VISIBLE);
+        } else {
+            noListText.setVisibility(View.GONE);
+        }
     }
 }
