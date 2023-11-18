d93fb4ca50e370b9e705859efe476d19af7b1c09
Ritam Singal <ritamsingal@gmail.com>
2023-11-17 20:22:52 -0800

    Added Harshil's description for API tests to the backend tests

--- backend/controllers/__tests__/listController.test.js
+++ backend/controllers/__tests__/listController.test.js
@@ -24,9 +24,15 @@ jest.mock('../../services/listService.js', () => ({
   createScheduleForList: jest.fn()
 }));
 
-describe('/:id getListById test', () => {
+// Interface GET /lists/:id
+describe('GET /:id getListById test', () => {
   let listId = 'mock-list-id';
 
+  // Input: valid listId
+  // Expected Status Code: 200
+  // Expected Behaviour: Returns the list with given id
+  // Expected Output: The list object
+  // ChatGPT usage: Yes
   it('should return a list', async () => {
     listService.getListById.mockResolvedValue({
       _id: listId,
@@ -36,15 +42,31 @@ describe('/:id getListById test', () => {
 
     const res = await request.get(`/lists/${listId}`);
     expect(res.status).toBe(200);
+    expect(res.body.list).toStrictEqual({
+      _id: listId,
+      listName: 'testList',
+      places: []
+    });
   });
 
+  // Input: invalid listId
+  // Expected Status Code: 400
+  // Expected Behaviour: Fails to get list
+  // Expected Output: Invalid listId Error message
+  // ChatGPT usage: Yes
   it('list id not found', async () => {
     listService.getListById.mockResolvedValue(null);
 
     const res = await request.get(`/lists/${listId}`);
     expect(res.status).toBe(400);
+    expect(res.body.errorMessage).toBe(`Failed to find list with ID: ${listId}`);
   });
 
+  // Input: valid listId
+  // Expected Status Code: 500
+  // Expected Behaviour: Internal server error
+  // Expected Output: Returns a 500 status
+  // ChatGPT usage: Yes
   it('list service throw error', async () => {
     listService.getListById.mockRejectedValue(new Error('mock error'));
 
@@ -53,17 +75,29 @@ describe('/:id getListById test', () => {
   });
 });
 
+// Interface POST /lists/create
 describe('POST /create create a new list', () => {
   const mockListName = 'mock-list-name';
   const mockListId = 'mock-list-id';
 
+  // Input: valid listName
+  // Expected Status Code: 200
+  // Expected Behaviour: Returns the listId
+  // Expected Output: The listId of new list
+  // ChatGPT usage: No
   it('should return a 200 status code on successful creation of new list', async () => {
     listService.createList.mockResolvedValue(mockListId);
 
     const res = await request.post(`/lists/create`).send({ listName: mockListName });
     expect(res.status).toBe(200);
+    expect(res.body.listId).toBe(mockListId);
   });
 
+  // Input: valid listName
+  // Expected Status Code: 500
+  // Expected Behaviour: Internal server error
+  // Expected Output: Returns a 500 status
+  // ChatGPT usage: No
   it('should return a 500 status code if something goes wrong during creation', async () => {
     listService.createList.mockRejectedValue(new Error('mock error'));
 
@@ -72,18 +106,30 @@ describe('POST /create create a new list', () => {
   });
 });
 
+// Interface PUT /lists/:id/add/place
 describe('PUT /:id/add/place add place to a list', () => {
   const mockPlace = { displayName: 'mock-display-name' };
   const mockListId = 'mock-list-id';
   const mockSuccessResult = 'mock-success-result';
 
+  // Input: valid place
+  // Expected Status Code: 200
+  // Expected Behaviour: Adds the place to list with given lisId
+  // Expected Output: Success Message
+  // ChatGPT usage: No
   it('should return a 200 status code on successful addition of place', async () => {
     listService.addPlaceToList.mockResolvedValue(mockSuccessResult);
 
     const res = await request.put(`/lists/${mockListId}/add/place`).send({ place: mockPlace });
     expect(res.status).toBe(200);
+    expect(res.body.message).toBe('Successfully added place to list');
   });
 
+  // Input: valid place
+  // Expected Status Code: 500
+  // Expected Behaviour: Internal server error
+  // Expected Output: Returns a 500 status
+  // ChatGPT usage: No
   it('should return a 500 status code if something goes wrong during addition', async () => {
     listService.addPlaceToList.mockRejectedValue(new Error('mock error'));
 
@@ -92,18 +138,30 @@ describe('PUT /:id/add/place add place to a list', () => {
   });
 });
 
+// Interface PUT /lists/:id/remove/place
 describe('PUT /:id/remove/place remove place from a list', () => {
   const mockPlaceId = 'mock-place-id';
   const mockListId = 'mock-list-id';
   const mockSuccessResult = 'mock-success-result';
 
+  // Input: valid placeId
+  // Expected Status Code: 200
+  // Expected Behaviour: Removes the place with given placeId from list with given lisId
+  // Expected Output: Success Message
+  // ChatGPT usage: No
   it('should return a 200 status code on successful removal of place', async () => {
     listService.removePlaceFromList.mockResolvedValue(mockSuccessResult);
 
     const res = await request.put(`/lists/${mockListId}/remove/place`).send({ placeId: mockPlaceId });
     expect(res.status).toBe(200);
+    expect(res.body.message).toBe('successfully removed place from list');
   });
 
+  // Input: valid placeId
+  // Expected Status Code: 500
+  // Expected Behaviour: Internal server error
+  // Expected Output: Returns a 500 status
+  // ChatGPT usage: No
   it('should return a 500 status code if something goes wrong during removal', async () => {
     listService.removePlaceFromList.mockRejectedValue(new Error('mock error'));
 
@@ -112,17 +170,29 @@ describe('PUT /:id/remove/place remove place from a list', () => {
   });
 });
 
+// Interface GET /lists/:id/places
 describe('GET /:id/places get all places for a list', () => {
   const mockPlaces = [{ displayName: 'mock-place-1' }, { displayName: 'mock-place-2' }];
   const mockListId = 'mock-list-id';
 
+  // Input: valid listId
+  // Expected Status Code: 200
+  // Expected Behaviour: Returns the places from list with given lisId
+  // Expected Output: places array
+  // ChatGPT usage: No
   it('should return a 200 status code on successful removal of place', async () => {
-    listService.getPlacesByListId.mockResolvedValue(mockPlaces);
+    listService.getPlacesByListId.mockResolvedValue({ places: mockPlaces });
 
     const res = await request.get(`/lists/${mockListId}/places`);
     expect(res.status).toBe(200);
+    expect(res.body.places).toStrictEqual(mockPlaces);
   });
 
+  // Input: valid listId
+  // Expected Status Code: 500
+  // Expected Behaviour: Internal server error
+  // Expected Output: Returns a 500 status
+  // ChatGPT usage: No
   it('should return a 500 status code if something goes wrong during removal', async () => {
     listService.getPlacesByListId.mockRejectedValue(new Error('mock error'));
 
@@ -131,17 +201,29 @@ describe('GET /:id/places get all places for a list', () => {
   });
 });
 
+// Interface PUT /lists/:id/add/schedule
 describe('PUT /:id/add/schedule add schedule for a list', () => {
   const mockSchedule = [{ displayName: 'mock-place-1' }, { displayName: 'mock-place-2' }];
   const mockListId = 'mock-list-id';
 
+  // Input: valid listId
+  // Expected Status Code: 200
+  // Expected Behaviour: Returns the schedule for list with given lisId
+  // Expected Output: schedule array
+  // ChatGPT usage: No
   it('should return a 200 status code on successful creation of schedule', async () => {
     listService.createScheduleForList.mockResolvedValue(mockSchedule);
 
     const res = await request.put(`/lists/${mockListId}/add/schedule`);
     expect(res.status).toBe(200);
+    expect(res.body.schedule).toStrictEqual(mockSchedule);
   });
 
+  // Input: valid listId
+  // Expected Status Code: 500
+  // Expected Behaviour: Internal server error
+  // Expected Output: Returns a 500 status
+  // ChatGPT usage: No
   it('should return a 500 status code if something goes wrong during schedule creation', async () => {
     listService.createScheduleForList.mockRejectedValue(new Error('mock error'));
 
@@ -150,17 +232,29 @@ describe('PUT /:id/add/schedule add schedule for a list', () => {
   });
 });
 
+// Interface DELETE /lists/:id/delete
 describe('DELETE /:id/delete delete a list by id', () => {
   const mockListId = 'mock-list-id';
   const mockSuccessResult = 'mock-success-result';
 
+  // Input: valid listId
+  // Expected Status Code: 200
+  // Expected Behaviour: Deletes the list with given lisId
+  // Expected Output: Success Message
+  // ChatGPT usage: No
   it('should return a 200 status code on successful deletion', async () => {
     listService.deleteListById.mockResolvedValue(mockSuccessResult);
 
     const res = await request.delete(`/lists/${mockListId}/delete`);
     expect(res.status).toBe(200);
+    expect(res.body.message).toBe('List successfully deleted');
   });
 
+  // Input: valid listId
+  // Expected Status Code: 500
+  // Expected Behaviour: Internal server error
+  // Expected Output: Returns a 500 status
+  // ChatGPT usage: No
   it('should return a 500 status code if something goes wrong during deletion', async () => {
     listService.deleteListById.mockRejectedValue(new Error('mock error'));
 
